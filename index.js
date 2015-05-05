9de53bf37b587fbf56ce34d0151e9bd448e126d4
Eduardo García Sanz <edgarcia@vectorsf.com>
2015-05-05 19:35:11 +0200

    working

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-module.exports = function reactify(filename, options) {
+var through = require('through'),
+    SVGO    = require('svgo'),
+    react   = require('react-tools');
 
-    
+var isSVG = function (filename) {
+
+    return (/\.svg$/i).test(filename);
+};
+
+var svgo = new SVGO();
+
+module.exports = function (filename) {
+
+    var write = function (buffer) {
+
+        data += buffer;
+    };
+
+    var end = function () {
+
+        svgo.optimize(data, out);
+    };
+
+    var out = function (svg) {
+
+        var source = "var React = require('react');module.exports = React.createClass({render: function () { return (" + svg.data + "); }});",
+            output = react.transform(source, {
+                es5           : true,
+                sourceMap     : false,
+                sourceFilename: filename,
+                stripTypes    : false,
+                harmony       : false
+            });
+
+        stream.queue(output);
+        stream.queue(null);
+    };
+
+    var data   = '',
+        stream = isSVG(filename) ? through(write, end) : through();
+
+    return stream;
 };
