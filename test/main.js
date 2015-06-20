8e8dd98ee65cfefc15ce2a554fab61df1c5addbe
Eduardo García Sanz <edgarcia@vectorsf.com>
2015-06-20 12:38:40 +0200

    SVG is just that, the SVG without the span wrapper [BRAKING CHANGE]

--- test/main.js
+++ test/main.js
@@ -3,14 +3,9 @@ var React     = require('react/addons'),
     fs        = require('fs'),
     vm        = require('vm'),
     path      = require('path'),
-    svgrt     = require('..');
+    svgrt     = require('..'),
+    jsdom     = require('jsdom');
 
-
-// file: test/setup.js
-var jsdom = require('jsdom');
-
-// A super simple DOM ready for React to render into
-// Store this DOM and the window in global scope ready for React to access
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 global.navigator = window.navigator;
